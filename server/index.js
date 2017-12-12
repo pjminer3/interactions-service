4194904aea704dc19092982a1814b276c7094226
Patrick Miner <patrickminer318@gmail.com>
2017-12-12 14:18:01 -0800

    Added dotenv, .env file, and customized .travis.yml file

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Path = require('path');
+const dotenv = require('dotenv');
 
-const PORT = 3000;
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyParser.json());
