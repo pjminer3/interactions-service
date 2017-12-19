a18d7d5df7c7445b6e20bd5a7ab838148647be27
Patrick Miner <patrickminer318@gmail.com>
2017-12-19 15:45:06 -0800

    Refactored generateInteraction code for better understanding

--- server/helperFunctions/generateInteraction.js
+++ server/helperFunctions/generateInteraction.js
@@ -2,31 +2,20 @@ const addIntsToDB = require('./addIntsToDB');
 
 const generateInteraction = (userId, tweetId, isAd, friendly) => {
   const prob = Math.random();
-  if (isAd) {
-    if (friendly) {
-      if (prob <= 0.075) {
-        // if its an ad friends have interacted with => 7.5% chance for interaction
-        return addIntsToDB(userId, tweetId, isAd, friendly);
-      }
-    } else {
-      if (prob <= 0.025) {
-        // if its an ad friends have NOT interacted with => 2.5% chance for interaction
-        return addIntsToDB(userId, tweetId, isAd, friendly);
-      }
-    }
-  } else {
-    if (friendly) {
-      if (prob <= 0.3) {
-        // if its a regular tweet that friends have interacted with => 30% chance for interaction
-        return addIntsToDB(userId, tweetId, isAd, friendly);
-      }
-    } else {
-      if (prob <= 0.1) {
-        // if its a regular tweet that friends have NOT interacted with => 10% chance for interaction
-        return addIntsToDB(userId, tweetId, isAd, friendly);
-      }
-    }
+  if (isAd && friendly && prob <= 0.075) {
+    // if its an ad friends have interacted with => 7.5% chance for interaction
+    return addIntsToDB(userId, tweetId, isAd, friendly);
+  } else if (isAd && prob <= 0.025) {
+    // if its an ad friends have NOT interacted with => 2.5% chance for interaction
+    return addIntsToDB(userId, tweetId, isAd, friendly);
+  } else if (friendly && prob <= 0.3) {
+    // if its a regular tweet that friends have interacted with => 30% chance for interaction
+    return addIntsToDB(userId, tweetId, isAd, friendly);
+  } else if (prob <= 0.1) {
+    // if its a regular tweet that friends have NOT interacted with => 10% chance for interaction
+    return addIntsToDB(userId, tweetId, isAd, friendly);
   }
+
   // if there is no interaction
   return Promise.resolve();
 };
