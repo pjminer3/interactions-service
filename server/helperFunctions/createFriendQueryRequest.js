7cf967d3931b78030c3c96727787719985901575
Patrick Miner <patrickminer318@gmail.com>
2018-01-01 17:56:40 -0800

    Begin: Add functions instead of endpoints

--- server/helperFunctions/createFriendQueryRequest.js
+++ server/helperFunctions/createFriendQueryRequest.js
@@ -3,20 +3,6 @@ const generateInteraction = require('./generateInteraction');
 
 // recursive function to create calls to Aygerim's service
 module.exports = function createFriendQueryRequest(response, tweets) {
-  // ---- THE BELOW IS AN OLD WAY THAT SENDS 1 REQUEST PER TWEET TO SOCIAL GRAPH SERVICE ----
-  // returns an iterable array of request promises to Social Graph Service
-  // return Promise.all(tweets.map((tweet) => {
-  //   return getFriendlyBoolean(response.data.user_id, tweet.tweet_id, tweet.isad)
-  //     .then((res) => {
-  //       return generateInteraction(response.data.user_id, tweet.tweet_id, tweet.isad, res.data);
-  //     });
-  // }))
-  //   .catch((err) => {
-  //     console.log('There was an error retreiving friend boolean: ', err);
-  //     return Promise.reject();
-  //   });
-  // ----------------------------------------------------------------------------------------
-  // --- NEW WAY THAT SENDS 1 REQUEST TO AYGERIM FOR THE ENTIRE BATCH OF TWEETS -------------
   // create the array of tweets
   const arrayOfTweetIds = [];
   const arrayOfIsAds = [];
@@ -38,8 +24,7 @@ module.exports = function createFriendQueryRequest(response, tweets) {
         .catch((err) => {
           console.log('There was an error in generating interactions');
           return Promise.reject();
-        })
-        // generateInteraction(response.data.user_id, tweet.tweet_id, tweet.isad, res.data);
+        });
     })
     .catch((err) => {
       console.log('There was an error retreiving friend boolean: ', err);
